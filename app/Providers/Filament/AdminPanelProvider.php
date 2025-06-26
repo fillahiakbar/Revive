ef5e9bee14ceebe1f805abc838a7d8b7b93cad47
fillahiakbar <fillahi099q@gmail.com>
2025-06-27 00:04:23 +0700

    FIX Login

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,8 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login()
+            ->login('admin.login')
+            ->authGuard('admin')
             ->colors([
                 'primary' => Color::Amber,
             ])
